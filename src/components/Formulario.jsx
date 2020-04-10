31851db3b1ffadbd670c65b48912f9dc83c99eb1
freilinjb <freilinjb@gmail.com>
2020-04-10 00:23:43 -0400

    Creando metodos submit y probando set handleChange

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Formulario = () => {
+
+    //state del formulario
+    const [busqueda, setBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    });
+
+    //funcion que colona los elementos en el state
+    const handleChange = e => {
+        //actualizar el state
+        setBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
+
+    const { ciudad, pais } = busqueda;
     return ( 
         <form>
             <div className="form-group">
-              <input type="text" name="ciudad" id="ciudad" className="form-control" placeholder="Ciudad" aria-describedby="helpId"/>
+              <input type="text" name="ciudad" onChange={handleChange} value={ciudad} id="ciudad" className="form-control" placeholder="Ciudad" aria-describedby="helpId"/>
             </div>
             <div className="form-group">
-              <select className="form-control" name="pais" id="pais" placeholder="País">
+              <select className="form-control" onChange={handleChange} name="pais" value={pais} id="pais" placeholder="País">
                 <option value="">--Seleccione un país</option>
-                <option></option>
-                <option></option>
+                <option value="DO">República Dominicana</option>
+                <option value="US">Estados Unidos</option>
+                <option value="MX">México</option>
+                <option value="AR">Argentina</option>
+                <option value="CO">Colombia</option>
+                <option value="CR">Costa Rica</option>
+                <option value="ES">España</option>
+                <option value="PE">Perú</option>
               </select>
             </div>
         </form>
